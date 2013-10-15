54250f3a3f26613d348f7b45868353de4f1953fc
mganley <mganley@juniper.net>
2013-10-15 16:05:16 -0700

    Added support for skipping the project if it is not on github. Also added support for taking into account the "revision". We make the assumption that the "revision" is specifying a branch.
    Tested this on my github account.

--- tools/packaging/scripts/fix-manifest.py
+++ tools/packaging/scripts/fix-manifest.py
@@ -2,11 +2,12 @@
 #
 
 import github
-import lxml.etree as ET
+from lxml import etree as ET
 import base64
 
 
 privates = ["contrail-vnc-private" ]
+DEBUG=0
 
 
 # Get the existing manifest from github
@@ -14,29 +15,38 @@ privates = ["contrail-vnc-private" ]
 
 
 # REPLACE WITH YOUR USER ID AND KEY
-agh = github.Github('mganley', '<insert key here>')
+agh = github.Github('mganley', '83236c3a4678abeda0cf2235552cac8a6b75ab9f')
 
 repo = agh.get_user(login="Juniper").get_repo(name=privates[0])
 contentFile = repo.get_contents("default.xml")
 
 # print contentFile.encoding
-root = ET.fromstring (base64.b64decode(contentFile.content))
+if DEBUG:
+	root = ET.parse("default.xml").getroot()
+else:
+	root = ET.fromstring (base64.b64decode(contentFile.content))
 
 
 for demo in root.iter('project'):
 	reponame = demo.get('name')
-	repo = agh.get_user(login="Juniper").get_repo(name=reponame)
-	print repo.name
-	#print repo.updated_at.strftime("%A, %d. %B %Y %I:%M%p")
-	#print "last commit to repo"
-	commits=repo.get_commits()[0]
-	#print commits.sha
-	#print commits.committer.login
-	demo.set ('version', commits.sha)
-	#print "\n"
+	remote = demo.get('remote')
+	print reponame, remote
+	if remote == "github" :
+		repo = agh.get_user(login="Juniper").get_repo(name=reponame)
+		print repo.name
+		#print repo.updated_at.strftime("%A, %d. %B %Y %I:%M%p")
+		dbranch = demo.get('revision')
+		if (dbranch) :
+			githubBranch = repo.get_branch(branch=dbranch)
+			commits=githubBranch.commit
+		else:
+			commits=repo.get_commits()[0]
+			#print commits.sha
+			#print commits.committer.login
+			#print "\n"
+		demo.set ('revision', commits.sha)
 
-
-# print ET.tostring(root, pretty_print = True)
+print ET.tostring(root, pretty_print = True)
 tree = ET.ElementTree(root)
 tree.write ('ec-manifest.xml')
 
