899258bbf58e39216d439e5b941b2e076f6fd69c
mganley <mganley@juniper.net>
2013-12-20 18:07:52 -0800

    Changed the logic.
    No longer want to fetch the manifest.xml from the Contrail
    repository.  Want to just locally process the manifest.xml file
    
    Let the Pipeline decide which repository and name of the
    manifest file.
    
    Changed the DEBUG flag to determine which user to look for the repositories
    on Github

--- tools/scripts/fix-manifest.py
+++ tools/scripts/fix-manifest.py
@@ -10,29 +10,38 @@ privates = ["contrail-vnc-private" ]
 DEBUG=0
 
 
+if DEBUG:
+    github_user="mganley"
+else:
+    github_user="Juniper"
+
 # Get the existing manifest from github
 # and update with sha hashes
 
 
 # REPLACE WITH YOUR USER ID AND KEY
+
 agh = github.Github('mganley', '83236c3a4678abeda0cf2235552cac8a6b75ab9f')
 
-repo = agh.get_user(login="Juniper").get_repo(name=privates[0])
-contentFile = repo.get_contents("default.xml")
 
+# repo = agh.get_user(login=github_user).get_repo(name=privates[0])
+
+#
+# Don't want to fetch the manifest directly.   Will do that via 
+# repo init -u 
+# We will limit this tool to just manipulating the manifest.xml file
+# contentFile = repo.get_contents("default.xml")
 # print contentFile.encoding
-if DEBUG:
-	root = ET.parse("default.xml").getroot()
-else:
-	root = ET.fromstring (base64.b64decode(contentFile.content))
 
+# Now parse the manifest.xml file.
+root = ET.parse("manifest.xml").getroot()
 
 for demo in root.iter('project'):
 	reponame = demo.get('name')
 	remote = demo.get('remote')
 	print reponame, remote
 	if remote == "github" :
-		repo = agh.get_user(login="Juniper").get_repo(name=reponame)
+		repo = agh.get_user(login=github_user).get_repo(name=reponame)
 		print repo.name
 		#print repo.updated_at.strftime("%A, %d. %B %Y %I:%M%p")
 		dbranch = demo.get('revision')
