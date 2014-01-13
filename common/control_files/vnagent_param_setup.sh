8fed7d3a32d2d4bdbdf9a5fccfd0b9b29a1a58c4
Ignatious Johnson <ijohnson@juniper.net>
2014-01-12 19:33:58 -0800

    Fix for the bug#2227, removing the agnet_param.tmpl if exists and recreating.

--- common/control_files/vnagent_param_setup.sh
+++ common/control_files/vnagent_param_setup.sh
@@ -3,30 +3,35 @@
 OS_VER=$1
 
 CFG_FILE=/etc/contrail/agent_param.tmpl
-if [ ! -f $CFG_FILE ] ; then
-    LOG=/var/log/contrail.log
-    echo "LOG=$LOG" > $CFG_FILE
 
-    CONFIG=/etc/contrail/agent.conf
-    echo CONFIG=$CONFIG >> $CFG_FILE
-
-    prog=/usr/bin/vnswad
-    echo prog=$prog >> $CFG_FILE
-
-    kmod=/lib/modules/${OS_VER}/extra/net/vrouter/vrouter.ko
-    echo kmod=$kmod >> $CFG_FILE
-
-    pname=$(basename $prog)
-    echo pname=$pname >> $CFG_FILE
-
-    echo "LIBDIR=/usr/lib64" >> $CFG_FILE
-    echo "VHOST_CFG=/etc/sysconfig/network-scripts/ifcfg-vhost0" >> $CFG_FILE
-
-    dev=$(cat /etc/contrail/default_if)
-    echo dev=__DEVICE__ >> $CFG_FILE
-
-    LOGFILE=/var/log/contrail/vrouter.log
-    echo "LOGFILE=--log-file=${LOGFILE}" >> $CFG_FILE
-
-    echo "$(date): agent_param updated for this server." &>> $LOG
+if [ -f $CFG_FILE ] ; then
+    rm -f $CFG_FILE
 fi
+
+LOG=/var/log/contrail.log
+echo "LOG=$LOG" > $CFG_FILE
+
+CONFIG=/etc/contrail/agent.conf
+echo CONFIG=$CONFIG >> $CFG_FILE
+
+prog=/usr/bin/vnswad
+echo prog=$prog >> $CFG_FILE
+
+kmod=/lib/modules/${OS_VER}/extra/net/vrouter/vrouter.ko
+echo kmod=$kmod >> $CFG_FILE
+
+pname=$(basename $prog)
+echo pname=$pname >> $CFG_FILE
+
+echo "LIBDIR=/usr/lib64" >> $CFG_FILE
+echo "VHOST_CFG=/etc/sysconfig/network-scripts/ifcfg-vhost0" >> $CFG_FILE
+
+dev=$(cat /etc/contrail/default_if)
+echo dev=__DEVICE__ >> $CFG_FILE
+echo vgw_subnet_ip=__VGW_SUBNET_IP__ >> $CFG_FILE
+echo vgw_subnet_mask=__VGW_SUBNET_MASK__ >> $CFG_FILE
+
+LOGFILE=/var/log/contrail/vrouter.log
+echo "LOGFILE=--log-file=${LOGFILE}" >> $CFG_FILE
+
+echo "$(date): agent_param updated for this server." &>> $LOG
