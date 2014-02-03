a3d97d098746bc5b91df8905a50ab0dc3fe6fd2f
Ignatious Johnson <ijohnson@juniper.net>
2014-02-02 21:03:30 -0800

    derived class with overriden ks_build method for fedora and redhat packaging.

---
+++ build/libs/packager/redhat_packager.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+''' Generic Library for RedHat Packager scripts'''
+
+import logging
+
+from common import BasePackager
+
+log = logging.getLogger("pkg.%s" %__name__)
+
+
+class Packager(BasePackager):
+    ''' Redhat Packager '''
+    def ks_build(self):
+        self.setup_env()
+        self.make_pkgs()
+        self.verify_built_pkgs_exists(skips=['contrail-install-packages'])
+        self.copy_built_pkg_files(self.pkg_repo, skips=['contrail-install-packages'])
+        self.createrepo(self.pkg_repo)
+        self.create_tgz(self.pkgs_tgz, self.pkg_repo)
+        self.create_contrail_pkg()
+        self.verify_built_pkgs_exists(['contrail-install-packages'])        
+        self.copy_built_pkg_files(self.store, ['contrail-install-packages'])
+        self.copy_pkg_files(self.base_pkgs, self.pkg_repo)
+        self.create_log()
+        self.create_git_ids()
+        self.create_comps_xml()
+        ks_file = self.create_ks()
+        self.createrepo(self.pkg_repo, extraargs='-g comps.xml')
+        self.run_pungi(ks_file)
