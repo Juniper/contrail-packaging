1fb71f51230e93611d4b91af6c7d0921d86bc4af
Atul Moghe <amoghe@juniper.net>
2014-02-07 20:18:14 -0800

    putting correct version of redis-server and modified setup_ubuntu.sh

--- build/setup_ubuntu.sh
+++ build/setup_ubuntu.sh
@@ -8,17 +8,14 @@ cd /opt/contrail/contrail_install_repo; tar xvzf /opt/contrail/contrail_packages
 # create shell scripts and put to bin
 mkdir -p /opt/contrail/bin
 
-#install sources.list from /opt/contrail/ to /etc/apt/
-cp /opt/contrail/contrail_packages/sources.list /etc/apt/sources.list 
-#apt-get update
+cd /opt/contrail/contrail_install_repo
+DEBIAN_FRONTEND=noninteractive dpkg -i binutils_2.22-6ubuntu1.1_amd64.deb dpkg-dev_1.16.1.2ubuntu7.2_all.deb libdpkg-perl_1.16.1.2ubuntu7.2_all.deb make_3.81-8.1ubuntu1.1_amd64.deb patch_2.6.1-3_amd64.deb python-pip_1.0-1build1_all.deb python-pkg-resources_0.6.24-1ubuntu1_all.deb python-setuptools_0.6.24-1ubuntu1_all.deb
 
-# create local repo and add this repo on top of sources.list
-cd /etc/apt/
+# create repo with only local packages
 datetime_string=`date +%Y_%m_%d__%H_%M_%S`
 cp sources.list sources.list.$datetime_string
-echo "deb file:/opt/contrail/contrail_install_repo ./" > new_repo
-cat new_repo sources.list > new_sources.list
-mv new_sources.list sources.list 
+echo "deb file:/opt/contrail/contrail_install_repo ./" > local_repo
+cat local_repo > sources.list
 
 #scan pkgs in local repo and create Packages.gz
 cd /opt/contrail/contrail_install_repo
@@ -28,16 +25,8 @@ dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 cp /opt/contrail/contrail_packages/preferences /etc/apt/preferences 
 apt-get update
 
-#install python-software-properties and add havana, java and webupd8 repo
+#install python-software-properties and curl
 sudo apt-get -y install python-software-properties curl
-sudo add-apt-repository -y cloud-archive:havana
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo add-apt-repository -y ppa:nilarimogard/webupd8
-echo "deb http://www.apache.org/dist/cassandra/debian 11x main" |     sudo tee /etc/apt/sources.list.d/cassandra.list
-curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add
-
-apt-get update
-
 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install dpkg-dev=1.16.1.2ubuntu7.2
 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install python-pip=1.0-1build1
 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install python-pkg-resources=0.6.24-1ubuntu1
@@ -59,3 +48,17 @@ echo 'sun-java6-bin shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debcon
 echo 'sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+
+#additional sources list from stock needed if only contrail specific pkgs are in in local repo
+cd /etc/apt/
+#install sources.list from /opt/contrail/ to /etc/apt/
+cp /opt/contrail/contrail_packages/sources.list /etc/apt/sources.list 
+cat new_repo sources.list > new_sources.list
+mv new_sources.list sources.list 
+
+sudo add-apt-repository -y cloud-archive:havana
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo add-apt-repository -y ppa:nilarimogard/webupd8
+echo "deb http://www.apache.org/dist/cassandra/debian 11x main" |     sudo tee /etc/apt/sources.list.d/cassandra.list
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add
+apt-get update
