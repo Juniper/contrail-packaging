4d8a5bbcc1e6ac3428726c8a0cf426d78041a2e2
Atul Moghe <amoghe@juniper.net>
2014-03-14 13:43:32 -0700

    porting vhost0 interfaces changes from mainline to R1.05

---
+++ common/control_files/vnagent_param_setup_ubuntu.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+OS_VER=$1
+
+CFG_FILE=/etc/contrail/agent_param.tmpl
+
+if [ -f $CFG_FILE ] ; then
+    rm -f $CFG_FILE
+fi
+
+LOG=/var/log/contrail.log
+echo "LOG=$LOG" > $CFG_FILE
+
+CONFIG=/etc/contrail/agent.conf
+echo CONFIG=$CONFIG >> $CFG_FILE
+
+prog=/usr/bin/vnswad
+echo prog=$prog >> $CFG_FILE
+
+kmod=/lib/modules/${OS_VER}/extra/net/vrouter/vrouter.ko
+echo kmod=$kmod >> $CFG_FILE
+
+pname=$(basename $prog)
+echo pname=$pname >> $CFG_FILE
+
+echo "LIBDIR=/usr/lib64" >> $CFG_FILE
+echo "DEVICE=vhost0" >> $CFG_FILE
+
+echo dev=__DEVICE__ >> $CFG_FILE
+echo vgw_subnet_ip=__VGW_SUBNET_IP__ >> $CFG_FILE
+echo vgw_subnet_mask=__VGW_SUBNET_MASK__ >> $CFG_FILE
+
+LOGFILE=/var/log/contrail/vrouter.log
+echo "LOGFILE=--log-file=${LOGFILE}" >> $CFG_FILE
+
+echo "$(date): agent_param updated for this server." &>> $LOG
