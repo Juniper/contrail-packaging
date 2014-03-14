1ac4f1a25374d18f18bd864d9dbcb5485cd4ce5e
Atul Moghe <amoghe@juniper.net>
2014-03-14 01:04:56 -0700

    This checkin covers following: 1. Vhost cross-connect bug fix porting from bitbucket. 2. Added vnagent_param_setup_ubuntu.sh to generate agent_param 3. Pre-up script if-vhost0 for vhost0, create and add device if not present..-- To help vhost interface to be up without delay. 5. Added start scripts to be used in upstart file. File name is different in github (vrouter-pre-start.sh) 6. Added vrouter-functions.sh for common functions used by if-vhost0 and vrouter-pre-start.sh 7. Removed /etc/init.d/networking restart from pre-start script 8. Setup.py changes for creating agent_param for ubuntu

---
+++ common/control_files/vrouter-functions.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+source /etc/contrail/agent_param
+
+function pkt_setup () {
+    for f in /sys/class/net/$1/queues/rx-*
+    do
+        q="$(echo $f | cut -d '-' -f2)"
+        ((mask=1<<$q))
+        printf "%x" $mask > $f/rps_cpus
+    done
+}
+
+function insert_vrouter() {
+    grep $kmod /proc/modules 1>/dev/null 2>&1
+    if [ $? != 0 ]; then 
+        insmod $kmod
+        if [ $? != 0 ]
+        then
+            echo "$(date) : Error inserting vrouter module"
+            return 1
+        fi
+
+        if [ -f /sys/class/net/pkt1/queues/rx-0/rps_cpus ]; then
+            pkt_setup pkt1
+        fi
+        if [ -f /sys/class/net/pkt2/queues/rx-0/rps_cpus ]; then
+            pkt_setup pkt2
+        fi
+    fi
+
+    echo "$(date): Creating vhost interface: $DEVICE."
+    # for bonding interfaces
+    loops=0
+    while [ ! -f /sys/class/net/$dev/address ]
+    do
+        sleep 1
+        loops=$(($loops + 1))
+        if [ $loops -ge 60 ]
+        then
+            echo "Unable to look at /sys/class/net/$dev/address"
+            return 1
+        fi
+    done
+
+    DEV_MAC=$(cat /sys/class/net/$dev/address)
+    vif --create $DEVICE --mac $DEV_MAC
+    if [ $? != 0 ]
+    then
+       echo "$(date): Error creating interface: $DEVICE"
+    fi
+
+    vif --add $DEVICE --mac $DEV_MAC --vrf 0 --mode x --type vhost
+    if [ $? != 0 ]
+    then
+       echo "$(date): Error adding $DEVICE to vrouter"
+    fi
+
+    echo "$(date): Adding $dev to vrouter"
+    DEV_MAC=$(cat /sys/class/net/$dev/address)
+    vif --add $dev --mac $DEV_MAC --vrf 0 --mode x --type physical
+    if [ $? != 0 ]
+    then
+        echo "$(date): Error adding $dev to vrouter"
+    fi
+    return 0
+}
