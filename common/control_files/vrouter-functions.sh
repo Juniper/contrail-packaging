f3a9c099ca38f4af80ae68c035c0682602092f44
Atul Moghe <amoghe@juniper.net>
2014-03-14 09:58:36 -0700

    Fixing a corner case of device getting added when vnagent restarts, added a condition to add device only when it is not present.

--- common/control_files/vrouter-functions.sh
+++ common/control_files/vrouter-functions.sh
@@ -29,39 +29,38 @@ function insert_vrouter() {
         fi
     fi
 
-    echo "$(date): Creating vhost interface: $DEVICE."
-    # for bonding interfaces
-    loops=0
-    while [ ! -f /sys/class/net/$dev/address ]
-    do
-        sleep 1
-        loops=$(($loops + 1))
-        if [ $loops -ge 60 ]
-        then
-            echo "Unable to look at /sys/class/net/$dev/address"
-            return 1
+    # check if vhost0 is not present, then create vhost0 and $dev
+    if [ ! -L /sys/class/net/vhost0 ]; then
+        echo "$(date): Creating vhost interface: $DEVICE."
+        # for bonding interfaces
+        loops=0
+        while [ ! -f /sys/class/net/$dev/address ]
+        do
+            sleep 1
+            loops=$(($loops + 1))
+            if [ $loops -ge 60 ]; then
+                echo "Unable to look at /sys/class/net/$dev/address"
+                return 1
+            fi
+        done
+
+        DEV_MAC=$(cat /sys/class/net/$dev/address)
+        vif --create $DEVICE --mac $DEV_MAC
+        if [ $? != 0 ]; then
+            echo "$(date): Error creating interface: $DEVICE"
         fi
-    done
 
-    DEV_MAC=$(cat /sys/class/net/$dev/address)
-    vif --create $DEVICE --mac $DEV_MAC
-    if [ $? != 0 ]
-    then
-       echo "$(date): Error creating interface: $DEVICE"
-    fi
+        vif --add $DEVICE --mac $DEV_MAC --vrf 0 --mode x --type vhost
+        if [ $? != 0 ]; then
+            echo "$(date): Error adding $DEVICE to vrouter"
+        fi
 
-    vif --add $DEVICE --mac $DEV_MAC --vrf 0 --mode x --type vhost
-    if [ $? != 0 ]
-    then
-       echo "$(date): Error adding $DEVICE to vrouter"
-    fi
-
-    echo "$(date): Adding $dev to vrouter"
-    DEV_MAC=$(cat /sys/class/net/$dev/address)
-    vif --add $dev --mac $DEV_MAC --vrf 0 --mode x --type physical
-    if [ $? != 0 ]
-    then
-        echo "$(date): Error adding $dev to vrouter"
+        echo "$(date): Adding $dev to vrouter"
+        DEV_MAC=$(cat /sys/class/net/$dev/address)
+        vif --add $dev --mac $DEV_MAC --vrf 0 --mode x --type physical
+        if [ $? != 0 ]; then
+            echo "$(date): Error adding $dev to vrouter"
+        fi
     fi
     return 0
 }
