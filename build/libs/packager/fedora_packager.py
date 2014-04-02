e982d609c1ee2611e890c7dd9a833f39aab74fb2
Nagendra Chandran <npchandran@juniper.net>
2014-04-01 21:24:57 -0700

    Packager changes for all platforms

--- build/libs/packager/fedora_packager.py
+++ build/libs/packager/fedora_packager.py
@@ -6,22 +6,21 @@ import logging
 from common import BasePackager
 
 log = logging.getLogger("pkg.%s" %__name__)
-
-
+        
 class Packager(BasePackager):
     ''' Fedora Packager '''
-    def ks_build(self):
-        self.meta_pkgs = ['contrail-install-packages']
+    def exec_steps(self):
         self.setup_env()
         self.create_pkg_list_file()
         self.make_pkgs()
-        self.verify_built_pkgs_exists(skips=self.meta_pkgs)
-        self.copy_built_pkg_files(skips=self.meta_pkgs)
+        self.verify_built_pkgs_exists(skips=self.meta_pkg)
+        self.copy_built_pkg_files(skips=self.meta_pkg)
         self.createrepo()
         self.create_pkgs_tgz()
-        self.create_contrail_pkg(*self.meta_pkgs)
-        self.verify_built_pkgs_exists(self.meta_pkgs)
-        self.copy_built_pkg_files(self.meta_pkgs, extra_dirs=self.store)
+        self.create_contrail_pkg()
+        self.verify_built_pkgs_exists(self.meta_pkg)
+        self.copy_built_pkg_files(self.meta_pkg,
+                                  extra_dirs=self.store)
         self.create_comps_xml()
         ks_file = self.create_ks()
         self.createrepo(extraargs='-g comps.xml')
