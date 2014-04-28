0c78fd385e2fb67abb81652337a3ea86b3847d55
Michael Ganley <mganley@juniper.net>
2014-04-28 15:28:12 -0700

    Updated the link.sh file not to create the links to the contrail build tools because they are copied.

--- tools/ansible/auto/link.sh
+++ tools/ansible/auto/link.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-cd /usr/local/bin
-ln -s /volume/ssd-tools/bin/config-ansible .
-ln -s /volume/ssd-tools/bin/contrail-archive .
-ln -s /volume/ssd-tools/bin/contrail-build .
-ln -s /volume/ssd-tools/bin/contrail-manipulate-manifest .
-ln -s /volume/ssd-tools/bin/create-base-manifest .
-ln -s /volume/ssd-tools/bin/create-config .
-ln -s /volume/ssd-tools/bin/fix-manifest.py .
-ln -s /volume/ssd-tools/bin/merge-manifest .
+# No longer want to create links.
+# Copying the files over so folks can work disconnected.
+
+# cd /usr/local/bin
+# ln -s /volume/ssd-tools/bin/config-ansible .
+# ln -s /volume/ssd-tools/bin/contrail-archive .
+# ln -s /volume/ssd-tools/bin/contrail-build .
+# ln -s /volume/ssd-tools/bin/contrail-manipulate-manifest .
+# ln -s /volume/ssd-tools/bin/create-base-manifest .
+# ln -s /volume/ssd-tools/bin/create-config .
+# ln -s /volume/ssd-tools/bin/fix-manifest.py .
+# ln -s /volume/ssd-tools/bin/merge-manifest .
 
 mkdir -p /cs-shared/builder
 cd /cs-shared/builder
