048f7d3de43292b35633aaf3b830333bea8cc411
Michael Ganley <mganley@juniper.net>
2014-06-16 01:23:24 -0700

    Added timestamp parameter and logic to the fix-manifest.py code.
    Will now find the commit that is before the timestamp and use that hash
    in the manifest.

--- tools/scripts/fix-manifest.py
+++ tools/scripts/fix-manifest.py
@@ -7,14 +7,13 @@ from lxml import etree as ET
 import base64
 
 
-privates = ["contrail-vnc-private" ]
-DEBUG=0
-
+privates = ["contrail-vnc-private"]
+DEBUG = 1
 
 if DEBUG:
-    github_user="mganley"
+    github_user = "Juniper"
 else:
-    github_user="Juniper"
+    github_user = "Juniper"
 
 # Get the existing manifest from github
 # and update with sha hashes
@@ -37,34 +36,41 @@ agh = github.Github('mganley', '83236c3a4678abeda0cf2235552cac8a6b75ab9f')
 # Now parse the manifest.xml file.
 # Should be the first (and only) argument
 if len(sys.argv) < 2:
-	print "usage: fix-manifest.py <manifest.xml filename>"
-	exit(1)
+    print "usage: fix-manifest.py <manifest.xml filename>"
+    exit(1)
 
 root = ET.parse(str(sys.argv[1])).getroot()
-
+time = sys.argv[2]
 for demo in root.iter('project'):
-	reponame = demo.get('name')
-	remote = demo.get('remote')
-	print reponame, remote
-	if remote == "github" :
-		repo = agh.get_user(login=github_user).get_repo(name=reponame)
-		print repo.name
-		#print repo.updated_at.strftime("%A, %d. %B %Y %I:%M%p")
-		dbranch = demo.get('revision')
-		if (dbranch) :
-			githubBranch = repo.get_branch(branch=dbranch)
-			commits=githubBranch.commit
-		else:
-			commits=repo.get_commits()[0]
-			#print commits.sha
-			#print commits.committer.login
-			#print "\n"
-		demo.set ('revision', commits.sha)
+    reponame = demo.get('name')
+    remote = demo.get('remote')
+    print reponame, remote
+    if remote == "github":
+        repo = agh.get_user(login=github_user).get_repo(name=reponame)
+        #print repo.updated_at.strftime("%A, %d. %B %Y %I:%M%p")
+        dbranch = demo.get('revision')
+        if (dbranch):
+            githubBranch = repo.get_branch(branch=dbranch)
+            commits = githubBranch.commit
+        else:
+            commits = repo.get_commits()[0]
+        if DEBUG:
+            print commits.sha
+            print commits.commit.committer.date
+            print "\n"
+        while str(commits.commit.committer.date) > time:
+            commits = commits.parents[0]
+            if DEBUG:
+                print commits.sha
+                print commits.commit.committer.date
+                print "\n"
 
-print ET.tostring(root, pretty_print = True)
+
+        demo.set('revision', commits.sha)
+
+print ET.tostring(root, pretty_print=True)
 tree = ET.ElementTree(root)
-tree.write ('ec-manifest.xml')
-
+tree.write('ec-manifest.xml')
 
 print "Done"
 
