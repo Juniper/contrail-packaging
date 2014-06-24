b1f77e1c3bf32c16335a2374fa99aca7bf65d70c
Hari <haripk@juniper.net>
2014-06-24 16:36:11 +0530

    update core_pattern, which is being overwritten by apport; bug id: 1332356

--- common/control_files/vrouter-post-start.sh
+++ common/control_files/vrouter-post-start.sh
@@ -5,3 +5,6 @@ dev=$(cat /etc/contrail/agent.conf | \
 	xdoc = etree.parse(sys.stdin); \
 	print xdoc.find("./agent/eth-port/name").text')
 ip link set dev $dev up
+
+# apport updates the core pattern; overwrite here to use /var/crashes pattern
+echo "/var/crashes/core.%e.%p.%h.%t" > /proc/sys/kernel/core_pattern
