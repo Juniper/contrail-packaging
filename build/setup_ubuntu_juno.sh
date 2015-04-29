84458e93a845f8db2482cff3cdcd3a9f619a9c69
Nagendra Chandran <npchandran@juniper.net>
2015-04-29 03:10:22 -0700

    upgrade dependent packages to latest upstream in ubuntu1404-icehouse, ubuntu1404-juno, ubuntu1204-icehouse Closes-Bug: 1449924
    Change-Id: I0ff305b108445498d4220330a33fd5c5c496b6e5

--- build/setup_ubuntu_juno.sh
+++ build/setup_ubuntu_juno.sh
@@ -14,11 +14,7 @@ cd /opt/contrail/contrail_install_repo; tar xvzf /opt/contrail/contrail_packages
 mkdir -p /opt/contrail/bin
 
 cd /opt/contrail/contrail_install_repo
-DEBIAN_FRONTEND=noninteractive dpkg -i binutils_2.24-5ubuntu3_amd64.deb make_3.81-8.2ubuntu3_amd64.deb libdpkg-perl_1.17.5ubuntu5.3_all.deb dpkg-dev_1.17.5ubuntu5.3_all.deb patch_2.7.1-4ubuntu1_amd64.deb
-
-#DEBIAN_FRONTEND=noninteractive dpkg -i binutils_2.22-6ubuntu1.1_amd64.deb dpkg-dev_1.16.1.2ubuntu7.2_all.deb libdpkg-perl_1.16.1.2ubuntu7.2_all.deb make_3.81-8.1ubuntu1.1_amd64.deb patch_2.6.1-3_amd64.deb python-pip_1.0-1build1_all.deb python-pkg-resources_0.6.24-1ubuntu1_all.deb python-setuptools_0.6.24-1ubuntu1_all.deb
-
-DEBIAN_FRONTEND=noninteractive dpkg -i python3-pycurl_7.19.3-0ubuntu3_amd64.deb python3-software-properties_0.92.37.1_all.deb software-properties-common_0.92.37.1_all.deb unattended-upgrades_0.82.1ubuntu2_all.deb
+DEBIAN_FRONTEND=noninteractive dpkg -i binutils_*.deb make_*.deb libdpkg-perl_*.deb dpkg-dev_*.deb patch_*.deb
 
 cd /etc/apt/
 # create repo with only local packages
@@ -65,7 +61,6 @@ DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes --allow-unauthenticated in
 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes --allow-unauthenticated install contrail-setup
 
 # install ecdsa and fabric
-pip install --upgrade --no-deps --index-url='' /opt/contrail/python_packages/ecdsa-0.10.tar.gz
 pip install --upgrade --no-deps --index-url='' /opt/contrail/python_packages/Fabric-1.7.0.tar.gz
 
 #disabled sun-java-jre and sun-java-bin prompt during installation, add oracle license acceptance in debconf
