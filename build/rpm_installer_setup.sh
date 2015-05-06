6b25c11cf9456ffe146c15837dc0e9b317bcb08d
Ignatious Johnson Christopher <ijohnson@juniper.net>
2015-05-06 16:39:56 -0700

    Packaging Fabric-1.7.5 to solve https://github.com/fabric/fabric/issues/600
    Change-Id: I613b8a5d8a870de759346a51eb8c13e662c320ac

--- build/rpm_installer_setup.sh
+++ build/rpm_installer_setup.sh
@@ -6,9 +6,9 @@ yum -y --disablerepo=* remove python-crypto-2.0.1
 # Install python fabric packages 
 yum -y --disablerepo=* --enablerepo=contrail* install python-pip createrepo python-netaddr gcc python python-devel
 
-pip install /opt/contrail/python-packages/pycrypto-2.6.tar.gz
-pip install /opt/contrail/python-packages/paramiko-1.11.0.tar.gz
-pip install /opt/contrail/python-packages/Fabric-1.7.0.tar.gz
+pip install /opt/contrail/python-packages/pycrypto-*.tar.gz
+pip install /opt/contrail/python-packages/paramiko-*.tar.gz
+pip install /opt/contrail/python-packages/Fabric-*.tar.gz
 
 # Install contrail fabric package
 yum -y --disablerepo=* --enablerepo=contrail* install contrail-fabric-utils
--- build/setup_ubuntu1404_split_pkgs.sh
+++ build/setup_ubuntu1404_split_pkgs.sh
@@ -43,7 +43,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes --allow-unauthenticated in
 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes --allow-unauthenticated install contrail-setup
 
 # install ecdsa and fabric
-pip install --upgrade --no-deps --index-url='' /opt/contrail/python_packages/Fabric-1.7.0.tar.gz
+pip install --upgrade --no-deps --index-url='' /opt/contrail/python_packages/Fabric-*.tar.gz
 
 #disabled sun-java-jre and sun-java-bin prompt during installation, add oracle license acceptance in debconf
 echo 'sun-java6-plugin shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections
