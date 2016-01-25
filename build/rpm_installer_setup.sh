cf12723c49f67f323ceadc64998ada0fae7b4afe
Nagendra Chandran <npchandran@juniper.net>
2016-01-25 14:41:03 -0800

    pip removal in contrail cloud packages
    Change-Id: Icdcff687113aec779c099706ce027b020be80363
    Closes-Bug: 1537931

--- build/rpm_installer_setup.sh
+++ build/rpm_installer_setup.sh
@@ -4,11 +4,11 @@
 yum -y --disablerepo=* remove python-crypto-2.0.1
 
 # Install python fabric packages 
-yum -y --disablerepo=* --enablerepo=contrail* install python-pip createrepo python-netaddr gcc python python-devel
+yum -y install python-pip createrepo python-netaddr gcc python python-devel
 
 pip install /opt/contrail/python-packages/pycrypto-*.tar.gz
 pip install /opt/contrail/python-packages/paramiko-*.tar.gz
 pip install /opt/contrail/python-packages/Fabric-*.tar.gz
 
 # Install contrail fabric package
-yum -y --disablerepo=* --enablerepo=contrail* install contrail-fabric-utils
+yum -y install contrail-fabric-utils
