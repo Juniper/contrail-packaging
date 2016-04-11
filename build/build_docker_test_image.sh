b05334838a14c67855453ab6d945467fc37707c3
Vinay Mahuli <vmahuli@juniper.net>
2016-04-11 02:41:01 -0700

    changes to docker build
    Closes-Bug: 1568741
    Change-Id: I9616fd9e13d73a85d03f560644c2f1d0d3bb901e

--- build/build_docker_test_image.sh
+++ build/build_docker_test_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 to_build=${@:-'contrail-test contrail-test-ci'}
-TEST_ARTIFACT=${TEST_ARTIFACT:-$(readlink -f sb/build/artifacts_extra/contrail-test2-*~$1.tgz)}
-TEST_CI_ARTIFACT=${TEST_CI_ARTIFACT:-$(readlink -f sb/build/artifacts_extra/contrail-test-ci-*~$1.tgz)}
-FABRIC_UTILS_ARTIFACT=${FABRIC_UTILS_ARTIFACT:-$(readlink -f sb/build/artifacts_extra/contrail-fabric-utils-*~$1.tgz)}
-CONTRAIL_PACKAGE_DEB=${CONTRAIL_PACKAGE_DEB:-$(readlink -f sb/build/artifacts/contrail-install-packages*~$1_all.deb)}
-DOCKER_IMAGE_EXPORT_PATH=${DOCKER_IMAGE_EXPORT_PATH:-$(readlink -f sb/build/artifacts/)}
+TEST_ARTIFACT=${TEST_ARTIFACT:-$(readlink -f build/artifacts_extra/contrail-test2-*~$1.tgz)}
+TEST_CI_ARTIFACT=${TEST_CI_ARTIFACT:-$(readlink -f build/artifacts_extra/contrail-test-ci-*~$1.tgz)}
+FABRIC_UTILS_ARTIFACT=${FABRIC_UTILS_ARTIFACT:-$(readlink -f build/artifacts_extra/contrail-fabric-utils-*~$1.tgz)}
+CONTRAIL_PACKAGE_DEB=${CONTRAIL_PACKAGE_DEB:-$(readlink -f build/artifacts/contrail-install-packages*~$1_all.deb)}
+DOCKER_IMAGE_EXPORT_PATH=${DOCKER_IMAGE_EXPORT_PATH:-$(readlink -f build/artifacts/)}
 IPADDRESS=${IPADDRESS:-$(ip a show docker0 | awk '/inet / {split ($2,a,"/"); print a[1]}')}
 
 tmp=$(mktemp -d)
@@ -15,5 +15,6 @@ for build in $to_build; do
     bash $tmp/contrail-test-ci/install.sh docker-build --test-artifact $TEST_ARTIFACT \
 	--ci-artifact $TEST_CI_ARTIFACT --fab-artifact $FABRIC_UTILS_ARTIFACT \
 	-u ssh://${IPADDRESS}/${CONTRAIL_PACKAGE_DEB} --export $DOCKER_IMAGE_EXPORT_PATH $build
+    [ $? = 0 ] && touch ci_docker_build_successful 
 done
 rm -fr $tmp
