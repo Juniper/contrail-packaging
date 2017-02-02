4ddc63d81244e79a5a766966790299aafee49c21
nitishkrishna <nitishk@juniper.net>
2017-02-02 11:33:08 -0800

    Closes-Bug: #1660765 - Install issues due to libpython version change upstream
    The version changed to 0.3 upstream and this caused strict check install to fail
    Patch 2: added lt check for re-install same version
    
    Change-Id: Ie8c6ec334b03b4248a6b62a6c462630634f6c6ae

--- build/deb_server_manager_setup.sh
+++ build/deb_server_manager_setup.sh
@@ -262,7 +262,7 @@ if [ "$SM" != "" ]; then
   if [ "$installed_version" != ""  ]; then
       version_to_install=`ls /opt/contrail/contrail_server_manager/contrail-server-manager_* | cut -d'_' -f 4`
       set +e
-      comparison=`dpkg --compare-versions $version_to_install gt $installed_version`
+      comparison=`dpkg --compare-versions $installed_version lt $version_to_install`
       check_upgrade=`echo $?`
       set -e
       if [ $check_upgrade == 0 ]; then
@@ -282,7 +282,7 @@ if [ "$SM" != "" ]; then
 
   #TODO: To be Removed after local repo additions
   if [ ${rel[1]} == "14.04"  ]; then
-    apt-get --no-install-recommends -y install libpython2.7=2.7.6-8ubuntu0.2 >> $log_file 2>&1
+    apt-get --no-install-recommends -y install libpython2.7>=2.7.6-8ubuntu0.2 >> $log_file 2>&1
   fi
   apt-get -y --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install puppet-common="3.7.3-1puppetlabs1" puppetmaster-common="3.7.3-1puppetlabs1" >> $log_file 2>&1
   cp /opt/contrail/contrail_server_manager/puppet.conf /etc/puppet/
