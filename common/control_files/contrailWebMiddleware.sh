75b781734d2061378c44fbc1beb0dc46aff77edd
Atul Moghe <amoghe@juniper.net>
2017-02-09 04:46:00 +0000

    Modified contrail-webui.service and contrailWebServer.sh to
    make sure that webui service is up after reboot and setup.
    Closes-Bug: #1663115
    
    Change-Id: I94d907a10742972813896c71dabbe753b1d983e2

--- common/control_files/contrailWebMiddleware.sh
+++ common/control_files/contrailWebMiddleware.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cd /usr/src/contrail/contrail-webui;
-if command -v nodejs-contrail > /dev/null; then
-  node_exec=nodejs-contrail;
-else
-  echo "error: Failed dependencies: nodejs-contrail is needed";
-  exit;
-fi;
-$node_exec jobServerStart.js 0.0.0.0
+cd /usr/src/contrail/contrail-web-core;
+node jobServerStart.js
 
