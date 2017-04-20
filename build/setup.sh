c0a77994ae82ddc9f07e76e322dce5c123297068
Nagendra Maynattamai <npchandran@juniper.net>
2017-04-20 16:10:31 -0700

    Centos/Rhel: Priority Override for Obsolete packages Closes-Bug: 1649869
    Change-Id: I4632d6d6be2ee61b97729588f2744b65912e3a81

--- build/setup.sh
+++ build/setup.sh
@@ -33,5 +33,18 @@ cp /opt/contrail/contrail_packages/helpers/* /opt/contrail/bin/
 # Remove existing python-crypto-2.0.1 rpm.
 yum -y --disablerepo=* remove python-crypto-2.0.1
 
+# Priority Override for obsolete packages
+priorities_conf="/etc/yum/pluginconf.d/priorities.conf"
+[ -f $priorities_conf ] && \
+grep -qi "\[main\]" $priorities_conf && \
+! grep -q "check_obsoletes\s*=\s*1" $priorities_conf && \
+sed -i 's/\[main\]$/&\ncheck_obsoletes = 1/' $priorities_conf && \
+echo "PASS: Added Priority Override for Obsolete packages" || ( \
+grep -q "check_obsoletes\s*=\s*1" $priorities_conf && \
+echo "PASS: Priority Override for Obsolete packages already exists. Nothing to do" ) || ( \
+[ ! -f $priorities_conf ] && echo "WARNING: $priorities_conf not found" ) || ( \
+echo "WARNING: Couldnt add priority Override for Obsolete packages. Check..." && \
+cat $priorities_conf )
+
 #Install basic packages 
 yum -y install contrail-fabric-utils contrail-setup
